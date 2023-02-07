4dbea1def44096351f95a7e972446df8373c75ef
unknI0Ivvn <124616605+unknI0Ivvn@users.noreply.github.com>
2023-02-07 01:08:27 +0100

    Add array to program

--- src/main/java/Algoritms/Lesson4/Lesson4.java
+++
@@ -1,6 +0,0 @@
-package Algoritms.Lesson4;
-
-public class Lesson4 {
-
-
-}
---
+++ src/main/java/Algoritms/Lesson4/Lesson4MergeSort.java
@@ -0,0 +1,72 @@
+package Algoritms.Lesson4;
+
+import java.util.Arrays;
+
+public class Lesson4MergeSort {
+    public static void main(String[] args) {
+        int[] arrTwo = {23,5,233,3,2,28,4};
+        System.out.println("-------------------");
+        System.out.println("Merge sort before:"+Arrays.toString(arrTwo));
+        System.out.println("-------------------");
+        mergeSort(arrTwo,arrTwo.length);
+        System.out.println("-------------------");
+        System.out.println("Merge sort after :"+Arrays.toString(arrTwo));
+        System.out.println("-------------------");
+
+    }
+
+    private static void mergeSort(int[] array, int length) {
+        if (length < 2) {
+            return;
+        }
+        int mid = length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[length - mid];
+        for (int i = 0; i < mid; i++) {
+            left[i] = array[i];
+
+        }
+        for (int i = mid; i < length; i++) {
+            right[i - mid] = array[i];
+
+        }
+        mergeSort(left, mid);
+        mergeSort(right, length - mid);
+        mergeArray(left, right, array);
+
+
+    }
+
+    private static void mergeArray(int[] one, int[] two, int[] array) {
+
+        int indexOne = 0;
+        int indexTwo = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (indexOne < one.length && indexTwo < two.length) {
+                int elementOne = one[indexOne];
+                int elementTwo = two[indexTwo];
+                if (elementOne < elementTwo) {
+                    array[i] = elementOne;
+                    indexOne++;
+                } else {
+                    array[i] = elementTwo;
+                    indexTwo++;
+
+                }
+                continue;
+            }
+            if (indexOne < one.length) {
+                array[i] = one[indexOne];
+                indexOne++;
+            }
+            if (indexTwo < two.length) {
+                array[i] = two[indexTwo];
+                indexTwo++;
+            }
+
+        }
+
+
+
+}
+    }
